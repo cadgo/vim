5f047670052bd51c9dbc3d624c8d896a89528538
Carlos Diaz <cadgo.root@gmail.com>
2022-11-29 08:50:53 -0600

    Update autoinstall.sh
    fixing one apt install -y

--- autoinstall.sh
+++ autoinstall.sh
@@ -43,7 +43,7 @@ function InstallDocker(){
     apt install -y docker-ce docker-ce-cli containerd.io docker-compose
   fi
   if [[ "$COS" == "Ubuntu" ]]; then
-    apt-get install ca-certificates curl gnupg lsb-release
+    apt-get install -y ca-certificates curl gnupg lsb-release
     mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
